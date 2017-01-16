f546be80f8b80f951b5f784f3677622771a65621
espython <espython85@gmail.com>
2017-01-16 04:29:59 +0200

    finally itworks

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-app.post('/signup', function(req, res) {
-    var name = req.body.name;
-    var email = req.body.email;
-    console.log(req.files);
-    res.json(req.files);
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser')
+var path = require("path");
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.get('/signup', function(req, res) {
+    res.sendFile(path.join(__dirname + '/signup.html'));
 });
+
+app.post('/signup', urlencodedParser, function(req, res) {
+    var username = req.body.username;
+    var email = req.body.email;
+    var password = req.body.password;
+    console.log('Name: ' + username);
+    console.log('Email: ' + email);
+    console.log('Password: ' + password);
+
+    res.json(req.body);
+
+});
+
+app.listen(3000, function() {
+    console.log('listening on port 3000!');
+})
